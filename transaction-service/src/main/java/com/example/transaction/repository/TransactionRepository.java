b556fc356d7529c1fb36fa37fc96ca9f409a2182
Jsri0203 <gjayasri98@gmail.com>
2025-09-15 18:17:52 -0400

    Create TransactionRepository.java
    transactional repository

---
+++ transaction-service/src/main/java/com/example/transaction/repository/TransactionRepository.java
@@ -0,0 +1,8 @@
+package com.example.transaction.repository;
+
+import com.example.transaction.entity.TransactionEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TransactionRepository extends JpaRepository<TransactionEntity, UUID> {}
