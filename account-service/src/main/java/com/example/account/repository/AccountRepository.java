f2ce198e2f11fb2d360809e820dfd3d4a03e4d45
Jsri0203 <gjayasri98@gmail.com>
2025-09-15 18:05:18 -0400

    Create AccountRepository.java
    account repository

---
+++ account-service/src/main/java/com/example/account/repository/AccountRepository.java
@@ -0,0 +1,8 @@
+package com.example.account.repository;
+
+import com.example.account.entity.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AccountRepository extends JpaRepository<Account, UUID> {}
